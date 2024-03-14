cb3ffe1e82abf2080954ecc657cccf65f591154b
ARPHAM <121754526+ARPHAM@users.noreply.github.com>
2024-03-14 08:11:46 +0700

    Update MP3.java

--- src/MP3.java
+++ src/MP3.java
@@ -1,2 +1,8 @@
-public class MP3 {
+import java.time.Duration;
+
+public class MP3 extends Item{
+    private Duration duration;
+    MP3(String name, String description, String iD, float price) {
+        super(name, description, iD, price);
+    }
 }
