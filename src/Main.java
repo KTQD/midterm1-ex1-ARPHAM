98923c8899e48efccdac01845dbe5ae4557ab0c6
ARPHAM <121754526+ARPHAM@users.noreply.github.com>
2024-03-14 08:10:35 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        MP3 mp3;
+        MP3 mp3 = new MP3("1","2","3",4);
         mp3.showInfo();
 
-        Book book;
+        Book book = new Book("5","6","7",8,"9",10,"11");
         book.showInfo();
 
     }
