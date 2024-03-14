027d3eda0adfa0bcd4fd7043af102e29b4dedbd1
ARPHAM <121754526+ARPHAM@users.noreply.github.com>
2024-03-14 08:10:50 +0700

    Update Item.java

--- src/Item.java
+++ src/Item.java
@@ -1,2 +1,23 @@
 public class Item {
+    public String name;
+    private String description;
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    protected String iD;
+    float price;
+    public void showInfo() {
+    }
+    Item (String name, String description,String iD, float price) {
+        this.name = name;
+        this.description = description;
+        this.iD = iD;
+        this.price = price;
+    }
 }
+
+
+
