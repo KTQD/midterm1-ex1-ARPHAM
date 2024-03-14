3f417d4254770803150556c48dfe938cc3362647
ARPHAM <121754526+ARPHAM@users.noreply.github.com>
2024-03-14 08:11:16 +0700

    Update Book.java

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,11 @@
-public class Book {
+public class Book extends Item{
+        String author;
+        int numberOfPages;
+        String genre;
+    Book (String name, String description, String iD, float price, String author, int numberOfPages,String genre) {
+        super(name, description, iD, price);
+        this.author = author;
+        this.numberOfPages = numberOfPages;
+        this.genre = genre;
+    }
 }
